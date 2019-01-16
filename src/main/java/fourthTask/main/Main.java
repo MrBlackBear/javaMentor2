067f2363c58e15ba99e918d960616ef0382de227
MrBlackBear <proboostbiznes@gmail.com>
2019-01-16 23:02:46 +0300

    fourth task

---
+++ src/main/java/fourthTask/main/Main.java
@@ -0,0 +1,36 @@
+package fourthTask.main;
+
+import fourthTask.chat.WebSocketChatServlet;
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+import java.util.logging.Logger;
+
+/**
+Доработать
+ */
+public class Main {
+   private static final Logger log = Logger.getLogger(Main.class.getName());
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(8080);
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+
+        context.addServlet(new ServletHolder(new WebSocketChatServlet()), "/fourthTask/chat");
+
+        ResourceHandler resource_handler = new ResourceHandler();
+        resource_handler.setDirectoriesListed(true);
+        resource_handler.setResourceBase("public_html");
+
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[]{resource_handler, context});
+        server.setHandler(handlers);
+
+        server.start();
+        log.info("Server started");
+        server.join();
+    }
+}
